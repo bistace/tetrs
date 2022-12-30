b03ffd982c41d11d39e7ef8351adce5afbca6c9b
Benjamin Istace <benjamin.istace@gmail.com>
2022-12-30 13:33:42 +0100

    Position tetriminos

--- src/engine/board.rs
+++ src/engine/board.rs
@@ -1,3 +1,5 @@
+use cgmath::Vector2;
+
 #[derive(Clone, Copy)]
 pub(crate) enum CellState {
     Empty,
@@ -15,5 +17,26 @@ impl Board {
 
     pub(crate) fn empty() -> Self {
         Self([CellState::Empty; Self::CELL_COUNT])
+    }
+
+    pub fn is_inside(coords: Vector2<usize>) -> bool {
+        coords.x < Self::WIDTH && coords.y < Self::HEIGHT
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn bounds_check() {
+        let coords_out_x = Vector2::<usize>::new(10, 9);
+        let coords_out_y = Vector2::<usize>::new(9, 20);
+        let coords_in = Vector2::<usize>::new(9, 19);
+
+        assert_eq!(Board::is_inside(coords_out_x), false);
+        assert_eq!(Board::is_inside(coords_out_y), false);
+        assert_eq!(Board::is_inside(coords_in), true);
     }
 }
--- src/engine/tetrimino.rs
+++ src/engine/tetrimino.rs
@@ -1,4 +1,5 @@
 use cgmath::Vector2;
+use crate::engine::board::Board;
 
 pub(super) struct Tetrimino {
     ttype: TType,
@@ -25,10 +26,18 @@ impl Tetrimino {
 
         let mut coords: [Vector2<usize>; 4] = [Vector2::<usize>::new(0, 0); 4];
         for(Vector2::<isize>{x, y}, coord) in offsets.into_iter().zip(&mut coords) {
-            *coord = match (x.try_into(), y.try_into()) {
+            // Negatives bound-checking
+            let tmp = match (x.try_into(), y.try_into()) {
                 (Ok(x), Ok(y)) => Vector2::<usize>::new(x, y),
                 _ => return None,
             };
+
+            // Checks that the coords are not superior to the maximum value of x or y
+            if Board::is_inside(tmp) {
+                *coord = tmp;
+            } else {
+                return None;
+            }
         }
 
         Some(coords)
@@ -90,5 +99,36 @@ where S: std::ops::Neg<Output=S> {
             Rotation::E => Vector2::new( self.y, -self.x),
             Rotation::W => Vector2::new(-self.y,  self.x)
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn position_s_tetrimino() {
+        let s_north = Tetrimino {ttype: TType::S, position: Vector2::new(2, 3), rotation: Rotation::N};
+        let s_south = Tetrimino {ttype: TType::S, position: Vector2::new(2, 3), rotation: Rotation::S};
+        let s_east = Tetrimino {ttype: TType::S, position: Vector2::new(2, 3), rotation: Rotation::E};
+        let s_west = Tetrimino {ttype: TType::S, position: Vector2::new(2, 3), rotation: Rotation::W};
+
+        assert_eq!(
+            s_north.cells(),
+            Some([(1, 3), (2, 3), (2, 4), (3, 4)].map(Vector2::<usize>::from))
+        );
+        assert_eq!(
+            s_south.cells(),
+            Some([(3, 3), (2, 3), (2, 2), (1, 2)].map(Vector2::<usize>::from))
+        );
+        assert_eq!(
+            s_east.cells(),
+            Some([(2, 4), (2, 3), (3, 3), (3, 2)].map(Vector2::<usize>::from))
+        );
+        assert_eq!(
+            s_west.cells(),
+            Some([(2, 2), (2, 3), (1, 3), (1, 4)].map(Vector2::<usize>::from))
+        );
     }
 }
