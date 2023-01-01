ed4632394d43d7feaaf5ec163c9cc82454e3a033
Benjamin Istace <benjamin.istace@gmail.com>
2023-01-01 16:39:14 +0100

    init sdl2

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,10 +1,18 @@
 use crate::engine::Engine;
 
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
+
 pub struct Interface {
     engine: Engine,
 }
 
 impl Interface {
+    const WIDTH: u32 = 1024;
+    const HEIGHT: u32 = 1024;
+
     // Constructs a new Interface with a default engine
     pub fn new() -> Self {
         Self {
@@ -18,6 +26,37 @@ impl Interface {
     }
 
     pub fn run(&self) {
-        todo!("Not implemented")
+        let sdl_context = sdl2::init().expect("Failed to load SDL2.");
+        let video_subsystem = sdl_context.video().expect("Failed to get video subsystem.");
+
+        let window = video_subsystem
+            .window("Tet.rs", Self::WIDTH, Self::HEIGHT)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let mut canvas = window
+            .into_canvas()
+            .build()
+            .expect("Failed to build the canvas.");
+
+        let mut event_pump = sdl_context
+            .event_pump()
+            .expect("Failed to get the event loop.");
+
+        loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. } => return,
+                    _ => {}
+                }
+            }
+            // The rest of the game loop goes here...
+
+            canvas.set_draw_color(Color::RGB(0, 255, 255));
+            canvas.clear();
+            canvas.present();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
     }
 }
